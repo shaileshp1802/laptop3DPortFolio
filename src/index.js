0089f9bda92cca9ac1841fabcaa15cd93e3d1f8a
Shailesh Paliwal <shpaliwal2002@gmail.com>
2023-04-09 01:01:44 +0530

    Suspense Added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Canvas } from '@react-three/fiber';
 import Exp from './Exp';
 
+function Loading() {
+  return(
+  <>
+  <h2>🌀 Loading...</h2>
+  <p>It might take some time to load 3d model.</p>
+  </>)
+}
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Canvas>
-      <Exp />
-    </Canvas>
+    <Suspense fallback={<Loading />}>
+      <Canvas>
+        <Exp />
+      </Canvas>
+    </Suspense>
   </React.StrictMode>
 );
+
